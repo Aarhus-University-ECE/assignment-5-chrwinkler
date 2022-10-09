f2dbf600d15030f8328731b075773d30772c706d
chrwinkler <101516854+chrwinkler@users.noreply.github.com>
2022-10-09 16:50:38 +0200

    Add files via upload

--- jollyJumper.c
+++ jollyJumper.c
@@ -2,33 +2,36 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+int isJollyJumber(const int seq[], int size)
+{
+    /*post: answer to exercise 7.a*/
+    int i;
+    bool diffs_found[size - 1];
 
-
-int isJollyJumber(const int seq[], int size) {
-/*post: answer to exercise 7.a*/
-int i;
-bool diffs_found[size-1];
-
-for(i = 0; i < size; i++){
-    if(seq[i]<seq[i+1]){
-        for(int k = 0; seq[k] < seq[k+1]; k++){
-            diffs_found[i] = k;
-            
+    for (i = 0; i < size; i++)
+    {
+        if (seq[i] < seq[i + 1])
+        {
+            for (int k = 0; seq[k] < seq[k + 1]; k++)
+            {
+                diffs_found[i] = k;
+            }
         }
-    }
-    if(seq[i]>seq[i+1]){
-        for(int k = 1; seq[k] > seq[k+1]; k++){
-            diffs_found[i] = k;
+        if (seq[i] > seq[i + 1])
+        {
+            for (int k = 0; seq[k] > seq[k + 1]; k++)
+            {
+                diffs_found[i] = k;
+            }
         }
-    }
-      if(diffs_found[size] == seq[size]){
+        if (diffs_found[size] == seq[size - 1])
+        {
             return 1;
-            }
-            else{
-                return 0;
-            }
-}
-
+        }
+        else
+        {
+            return 0;
+        }
+    }
     return 1;
 }
-
--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "circle.h"
-#include"jollyjumper.h"
+#include "jollyjumper.h"
 
-
-void printCircle(circle c) {
+void printCircle(circle c)
+{
 	printf("p.x: %d, p.y: %d, r: %d\n", c.p.x, c.p.y, c.r);
 }
 
-int main(void) {
+int main(void)
+{
 	/*testing circles - make sure that you know how it works and what it is supposed to print out*/
 	circle c[5];
 	fiveCircles(c);
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5; i++)
+	{
 		printCircle(c[i]);
 	}
 	point p;
@@ -23,30 +25,32 @@ int main(void) {
 	printf("isValid: %d", circleIsValid(&c[1]));
 	printf("\n");
 
-
 	/*answer to exercise 7.b*/
 	int n; /*number of numbers to read*/
 	/*readin n and check that is is OK*/
 
 	printf(" Enter size of seqaunce;\n");
-	scanf("%d",&n);
-		
+	scanf("%d", &n);
 
 	int *numbers = malloc(sizeof(int) * n); /*the numbers read*/
 
-   int num;
+	int num;
 	printf(" Enter numbers of seqaunce;\n");
-	for(int i =0; i < n; i++){
-		scanf("%d",&num);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &num);
 		numbers[i] = num;
 	}
 
 	/*readin the n numbers in the array numbers*/
 
-	if (isJollyJumber(numbers, n)) {
-		printf("it is a Jolly Jumper");}
-	else {
-		printf("not a Jolly Jumper");}
+	if (isJollyJumber(numbers, n))
+	{
+		printf("it is a Jolly Jumper");
+	}
+	else
+	{
+		printf("not a Jolly Jumper");
+	}
 	return 0;
-
 }
